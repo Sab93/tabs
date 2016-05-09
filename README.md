9d65684706bd70c316994e82728107a947f9a1dc
stagiaire <stagiaire>
2016-05-09 16:29:14 +0200

    import initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tabs
 Tabs widget jQuery
+
+blabla
---
+++ js/tabs.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	$('#tabs ul li').click(function(){
+		var id= $(this).children().attr('href');
+
+		$('#tabs div.active').removeClass('active').toggle();
+		$('#tabs ul li.active').removeClass('active');
+
+		$(id).addClass('active').fadeToggle('fast');
+		$(this).addClass('active');
+
+	});
+
+
+});
